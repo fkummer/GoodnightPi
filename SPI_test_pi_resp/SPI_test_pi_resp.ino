975b18e1b573878777f30db3f771a54dbd4f30c8
fkummer <fck22@cornell.edu>
2016-11-18 16:57:20 -0500

    Working on SPI

--- SPI_test_pi_resp/SPI_test_pi_resp.ino
+++ SPI_test_pi_resp/SPI_test_pi_resp.ino
@@ -1,15 +1,16 @@
 // Written by Nick Gammon
 // April 2011
+#include <SPI.h>
 
-// what to do with incoming data
-volatile byte command = 0;
-
-unsigned char SPI_buff[11];
+volatile unsigned char SPI_buff[99];
+volatile byte cnt = 0;
 volatile byte pos = 0;
+volatile byte resp = 0;
 
 void setup (void)
 {
   Serial.begin(115200);
+  SPI.setClockDivider(SPI_CLOCK_DIV32);
   // have to send on master in, *slave out*
   pinMode(MISO, OUTPUT);
 
@@ -23,23 +24,40 @@ void setup (void)
 
 
 // SPI interrupt routine
+//Capture what is coming in. 
 ISR (SPI_STC_vect)
 {
-  if(pos < 11){
-    SPI_buff[pos] = SPDR;
-    pos = pos + 1;
-  }else{
-     pos = 0;
-     for(int i = 0; i < 11; i++){
-        Serial.print(SPI_buff[i]);
-     }
-     Serial.println('');
-    
+  if(pos > 98){
+    pos = 0;
   }
-
+  
+  cnt++;
+  SPI_buff[pos] = SPDR;
+  pos++;
+  
+  if(resp){
+    SPDR = 'Y';
+  }
+  
 }  // end of interrupt service routine (ISR) SPI_STC_vect
 
 void loop (void)
 {
-  
+  //Received a packet
+  if(cnt == 11 && !resp){  
+    Serial.println(pos);
+    for(int i = pos - 11; i < pos; i++){
+      Serial.write(SPI_buff[i]);
+    }
+    Serial.print("\n");
+    cnt = 0;
+    resp = 1;
+  }else{
+    if(cnt == 11 && resp){
+      //We sent back out our response, listen again
+      Serial.println("We did it reddit");
+      resp = 0;
+      cnt = 0;
+    }
+  }
 }  // end of loop
