8f0be36e4d9fbe164698d1cade9d7bf33aae9e21
fkummer <fck22@cornell.edu>
2016-11-29 18:53:33 -0500

    More SPI

--- SPI_test_pi_resp/SPI_test_pi_resp.ino
+++ SPI_test_pi_resp/SPI_test_pi_resp.ino
@@ -9,6 +9,7 @@ volatile byte resp = 0;
 volatile byte incoming = 0;
 volatile byte outgoing = 0;
 volatile unsigned char resp_buff[11];
+volatile byte resp_ind = 0;
 
 void setup (void)
 {
@@ -35,6 +36,8 @@ ISR (SPI_STC_vect)
     pos = 0;
   }
   
+  Serial.println(SPDR);
+  
   //If we are receiving a packet
   if(incoming){
     cnt++;
@@ -53,9 +56,14 @@ ISR (SPI_STC_vect)
     }
   }
   
+  
   //The master is asking us for a response and we expect to return one.
-  if(SPDR == 0xFF && outgoing){
-    SPDR = 'Y';
+  
+  //HERES THE PROBLEM: We are overwriting the SPDR, so we never bet in the 0xFF we want?
+  if(SPDR == 0xFF){
+    Serial.println("AH");
+    SPDR = resp_buff[resp_ind];
+    resp_ind++;
   }
   
 }  // end of interrupt service routine (ISR) SPI_STC_vect
@@ -66,22 +74,39 @@ void loop (void)
   //We should also get our response ready!
   if(cnt == 11){  
     //Print out the packet
-    Serial.println(pos);
-    byte ind = (pos-11)%100;
+    //Serial.println(pos);
+    byte ind;
+
+    ind = (pos-11)%100;
+    
     for(int i = 0; i < 11; i++){
       
       if(ind > 99){
         ind = 0;
       }
       //Serial.println(ind);
-      Serial.write(SPI_buff[ind]);
+      //Serial.write(SPI_buff[ind]);
       
       ind++;
     }
     Serial.print("\n");
     cnt = 0;
     
-    //Ready our response!
+
+    if(outgoing){
+      Serial.println("Prep out");
+      for(int k = 0; k<11;k++){
+        resp_buff[k] = 'Y';
+      }
+      
+      SPDR = resp_buff[0];
+      resp_ind = 1;
+      outgoing = 0;
+
+    }
+      
+      
+    
     
   }
 }
