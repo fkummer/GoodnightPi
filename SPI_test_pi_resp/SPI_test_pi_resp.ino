8b04c0dc771ac143a3e252adaf017528636132c3
fkummer <fck22@cornell.edu>
2016-11-30 01:28:05 -0500

    Slave response on arduino end working, as well as circular buffer

--- SPI_test_pi_resp/SPI_test_pi_resp.ino
+++ SPI_test_pi_resp/SPI_test_pi_resp.ino
@@ -11,6 +11,8 @@ volatile byte outgoing = 0;
 volatile unsigned char resp_buff[11];
 volatile byte resp_ind = 0;
 
+#define PACKET_LEN 11
+
 void setup (void)
 {
   Serial.begin(115200);
@@ -36,7 +38,7 @@ ISR (SPI_STC_vect)
     pos = 0;
   }
   
-  Serial.println(SPDR);
+  //Serial.println(SPDR);
   
   //If we are receiving a packet
   if(incoming){
@@ -44,13 +46,14 @@ ISR (SPI_STC_vect)
     //Serial.println(SPDR);
     SPI_buff[pos] = SPDR;
     pos++;
-    if(cnt >= 11){
+    if(cnt >= PACKET_LEN){
       incoming = 0;
       outgoing = 1;
     }
   }else{//Look for start byte
     if(SPDR == 0xFE){
       incoming = 1;
+      
       outgoing = 0;
       cnt = 0;
     }
@@ -59,33 +62,53 @@ ISR (SPI_STC_vect)
   
   //The master is asking us for a response and we expect to return one.
   
-  //HERES THE PROBLEM: We are overwriting the SPDR, so we never bet in the 0xFF we want?
+  //HERES THE PROBLEM: We are overwriting the SPDR so we can't get this 0xFF? Or is this pi sending us Y? Check on scope.
   if(SPDR == 0xFF){
-    Serial.println("AH");
+    //Serial.println("AH");
     SPDR = resp_buff[resp_ind];
     resp_ind++;
   }
   
 }  // end of interrupt service routine (ISR) SPI_STC_vect
 
+byte getOpCode(byte receivedOpCode){
+  switch (receivedOpCode){
+    case 0x00://Respond to WAKE_REQ with WAKE_RESP
+      return 0x01;
+      break;
+    case 0x02:
+      return 0x03;
+      break;
+    case 
+    
+      
 void loop (void)
 {
   //Received a full packet, lets print it
   //We should also get our response ready!
-  if(cnt == 11){  
+  if(cnt == PACKET_LEN){  
     //Print out the packet
-    //Serial.println(pos);
+    Serial.println(pos);
     byte ind;
-
-    ind = (pos-11)%100;
     
-    for(int i = 0; i < 11; i++){
+    if(pos < PACKET_LEN){
+      ind = 99-(pos+1);
+    }else{
+      ind = (pos-PACKET_LEN)%100;
+    }
+    Serial.println("Init ind");
+    Serial.println(ind);
+    //}else{
+    //  ind = ((pos-12)%100) ;
+    //}
+    
+    for(int i = 0; i < PACKET_LEN; i++){
       
       if(ind > 99){
         ind = 0;
       }
       //Serial.println(ind);
-      //Serial.write(SPI_buff[ind]);
+      Serial.write(SPI_buff[ind]);
       
       ind++;
     }
@@ -94,9 +117,16 @@ void loop (void)
     
 
     if(outgoing){
-      Serial.println("Prep out");
-      for(int k = 0; k<11;k++){
-        resp_buff[k] = 'Y';
+      //Serial.println("Prep out");
+      unsigned char resp_string[]
+    = {'l','m','n','o','p','q','r','s','t','u','\0'};
+      for(int k = 0; k<PACKET_LEN;k++){
+//        if(k%2 == 0){
+//          resp_buff[k] = 'Y';
+//        }else{
+//          resp_buff[k] = 'W';
+//        }
+          resp_buff[k] = resp_string[k];
       }
       
       SPDR = resp_buff[0];
